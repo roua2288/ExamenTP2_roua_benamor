baab064ab99cfba947a32a03dd5abadf0d9fd75c
roua2288 <167281795+roua2288@users.noreply.github.com>
2025-03-14 10:53:54 +0100

    Fusion squash de casee2 avec résolution de conflits

--- case.py
+++ case.py
@@ -5,6 +5,15 @@ def somme(T):
     S+=t
   return
 Data=[1,3,5]
+<<<<<<< HEAD
 Som=sum(Data)
 print('la somme est : ', Som)
+
+
+if Data:
+  print('La somme est :', sum(Data))
+  print('Le min est :', min(Data))
+  print('Le max est :', max(Data))
+else:
+  print('Dossier vide')
 
